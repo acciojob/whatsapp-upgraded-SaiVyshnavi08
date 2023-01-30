9e387d01feadcc8c2bb2d64ece999f0b56d45129
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2023-01-30 22:39:40 +0530

    WhatsApp

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -28,9 +28,7 @@ public class WhatsappController {
     public String createUser(String name, String mobile) throws Exception {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
-       if(whatsappService.UniqueNumber(mobile)==false){
-           throw new Exception("User already exists");
-       }
+
         return whatsappService.createUser(name, mobile);
     }
 
